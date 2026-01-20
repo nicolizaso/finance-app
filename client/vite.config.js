b3f1b7e49eda707d9e6ddb7e81ae515faef3a1f9
nicolizaso <nlizaso69@gmail.com>
2026-01-20 16:15:49 -0300

    limite de cache

--- client/vite.config.js
+++ client/vite.config.js
@@ -37,7 +37,10 @@ export default defineConfig({
             sizes: '512x512',
             type: 'image/png'
           }
-        ]
+        ],
+        workbox: {
+          maximumFileSizeToCacheInBytes: 5000000
+        },
       }
     })
   ],
